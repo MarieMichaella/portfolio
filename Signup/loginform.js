ba398264be714263cbdc0ede4bb8862117caf27c
MarieMichaella <101724107+MarieMichaella@users.noreply.github.com>
2023-02-13 16:25:36 +0200

    Add files via upload

---
+++ Signup/loginform.js
@@ -0,0 +1,79 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+const button = document.querySelector('button[type="submit"]');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+function validatePassword(password) {
+    return password.length > 8;
+  }
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    }  
+    else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'email is required');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } 
+    else if (passwordValue.length < 8)
+    {
+        setError(password, 'Your password must have more than 8 characters');
+    } 
+    else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Password is required');
+    } else if (password2Value != '' & password2Value!= passwordValue) {
+        setError(password2, 'Passwords do not match');
+    }
+     else {
+        setSuccess(password2);
+    }
+    if (usernameValue != '' && emailValue != '' && passwordValue != '' && password2Value === passwordValue) {
+        window.location.replace('/Dashboard/index1.html');
+      }
+};
